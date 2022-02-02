f5d4286564d0bfe22458bf4b07e2a8453a076fb0
buchegerb1704 <buchegerb1704@my.uwstout.edu>
2022-02-02 13:29:08 -0600

    Added lists for ingredients and drinks
    Still working on grabbing an item from the list.

--- DrinkData.cs
+++ DrinkData.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class DrinkData : Node
 {
@@ -15,8 +16,20 @@ public class DrinkData : Node
         }
     }
 
-    private void Display() {
+    private List<Ingredient> _ingredientList = new List<Ingredient>();
+    private List<Drink> _drinkList = new List<Drink>();
 
+    public void addIngredient(Ingredient ingredient) {
+        _ingredientList.Add(ingredient);
+    }
+    public void returnIngredient(Ingredient ingredient) {
+
+    }
+    public void addDrink(Drink drink) {
+        _drinkList.Add(drink);
+    }
+    public void returnDrink(Drink drink) {
+        
     }
 
     // Called when the node enters the scene tree for the first time.
