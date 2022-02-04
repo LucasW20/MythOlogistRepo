68ccff467a9a99b33641e18ce720dc725ac4c818
LucasW20 <lucas.wright552@gmail.com>
2022-02-03 23:08:13 -0600

    DrinkData Update & Order System Started
    Added 4 getter methods to DrinkData to get Ingredients and Drinks via index and the size of the lists. Added Order method to Order System to randomly decide which type of order and ingredient/drink is being ordered.

--- OrderSys.cs
+++ OrderSys.cs
@@ -5,11 +5,9 @@ using System;
  * Determines which kind of order and which drink is going to be ordered
  * @author Lucas_C_Wright
  * @start 2-2-2022
- * @version 2-2-2022
+ * @version 2-3-2022
  */
 public class OrderSys : Node {
-
-
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
@@ -29,17 +27,45 @@ public class OrderSys : Node {
      * Drink ordered
      */
     public void Order() {
+        //whenever this is called generate a new random object so the seed is updated
+        Random rand = new Random();
+
+        DrinkData inst = DrinkData.Instance;
+
         //randomly decide which order type is going to be processed
-            //if Two Ingredients then...
+        int type = rand.Next(0, 2);
+        switch (type) { 
+        case 0: //if Two Ingredients then...
                 //randomly choose a drink from the drink list
+                Drink correctDrink = inst.returnDrinkAt(rand.Next(0, inst.DrinkSize() - 1));
+
                 //get both ingredients used in that drink
+                Ingredient ing1 = correctDrink.getFirstIng();
+                Ingredient ing2 = correctDrink.getSecondIng();
+
                 //send ingredients and correct drink to the communication system
-            //if One Ingredient then...
+                //TODO
+                break;
+        case 1: //if One Ingredient then...
                 //randomly choose an ingredient from the ingredient list
+                Ingredient ing = inst.returnIngredientAt(rand.Next(0, inst.IngredientSize() - 1));
+
                 //send ingredient and list of correct drinks to the communication system
-            //if Drink then...
+                //TODO
+                break;
+        case 2: //if Drink then...
                 //randomly choose a drink from the drink list
+                Drink drink = inst.returnDrinkAt(rand.Next(0, inst.DrinkSize() - 1));
+
                 //send the drink to the communication system
+                //TODO
+                break;
+        default:
+                GD.Print("random type number not 0-2: " + type);
+                break;
+        }
+            
+                
     }
 
 }
