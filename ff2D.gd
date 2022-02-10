073b40bc8f5d2e65fc34d662fe38c2dc7064f4ad
schmitzs1 <aclassyfox44@gmail.com>
2022-02-10 08:11:30 -0600

    dragndrop

--- ff2D.gd
+++ ff2D.gd
@@ -4,18 +4,14 @@ extends Sprite
 var selected = false
 
 
+
 func _physics_process(delta):
 	
 	#print(global_position, get_global_mouse_position())
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
 
-func _on_Area2D_input_event(viewport, event, shape_idx):
+func _on_ff_2D_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("grab"):
 		print("working")
 		selected = true
-
-
-
-
-
