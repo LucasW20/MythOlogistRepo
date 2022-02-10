98cd57980632b57db751c16db13617189ef7706e
schmitzs1 <98604405+schmitzs1@users.noreply.github.com>
2022-02-10 11:03:05 -0600

    update drag n drop

--- ff2D.gd
+++ ff2D.gd
@@ -1,17 +1,41 @@
 extends Sprite
 
-
+var fruitjuice = preload("res://Images/Drinks/dr_16_forbiddenFruitJuice.png")
+onready var fruit = get_node(".")
 var selected = false
+var rest_point
+var rest_nodes = []
 
-
+func _ready():
+	rest_nodes = get_tree().get_nodes_in_group("zone")
+	
+	rest_point = rest_nodes[0].global_position
+	rest_nodes[0].select()
 
 func _physics_process(delta):
 	
 	#print(global_position, get_global_mouse_position())
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+	else:
+		global_position= lerp(global_position, rest_point, 10 * delta)
 
 func _on_ff_2D_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("grab"):
 		print("working")
 		selected = true
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			selected = false
+			var shortest_dist = 75
+			for child in rest_nodes:
+				var distance = global_position.distance_to(child.global_position)
+				if distance < shortest_dist:
+					child.select()
+					rest_point = child.global_position
+					shortest_dist = distance
+					print(name)
+					fruit.set_texture(fruitjuice)
+		
