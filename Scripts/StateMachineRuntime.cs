6965944f233e295d1d7f9409f594d0fcc377f874
schmitzs1 <aclassyfox44@gmail.com>
2022-02-11 14:47:57 -0600

    push for working

--- Scripts/StateMachineRuntime.cs
+++ Scripts/StateMachineRuntime.cs
@@ -9,120 +9,120 @@ using System.Collections.Generic;
  * @version 2-9-2022
  */
 public abstract class StateMachineRuntime : Node {
-    private string state;
-    private string previousState = "";
-    private int stateIndex;
-    List<string> states = new List<string>();
+	private string state;
+	private string previousState = "";
+	private int stateIndex;
+	List<string> states = new List<string>();
 
 
-    // Called when the node enters the scene tree for the first time.
-    //public StateMachineRuntime () {
-    //    //add all of the various states to the list
-    //    GD.Print("Created new StateMachineRuntime");
-    //    states.Add("Arrive");
-    //    states.Add("Order");
-    //    states.Add("Leave");
+	// Called when the node enters the scene tree for the first time.
+	//public StateMachineRuntime () {
+	//    //add all of the various states to the list
+	//    GD.Print("Created new StateMachineRuntime");
+	//    states.Add("Arrive");
+	//    states.Add("Order");
+	//    states.Add("Leave");
 
-    //    //set the state and start the logic
-    //    state = states[0];
-    //    stateIndex = 0;
-    //    stateLogic();
-    //}
+	//    //set the state and start the logic
+	//    state = states[0];
+	//    stateIndex = 0;
+	//    stateLogic();
+	//}
 
-    public override void _Ready() {
-        //add all of the various states to the list
-        GD.Print("Created new StateMachineRuntime");
-        states.Add("Arrive");
-        states.Add("Order");
-        states.Add("Leave");
+	public override void _Ready() {
+		//add all of the various states to the list
+		GD.Print("Created new StateMachineRuntime");
+		states.Add("Arrive");
+		states.Add("Order");
+		states.Add("Leave");
 
-        //set the state and start the logic
-        state = states[0];
-        stateIndex = 0;
-        stateLogic();
-    }
+		//set the state and start the logic
+		state = states[0];
+		stateIndex = 0;
+		//stateLogic();
+	}
 
 
-    //determines what happens when for each stage
-    public void stateLogic() {
-        //depending on the current state this decides which logic is going to play out.
-        switch (state) {
-            case "Arrive":
-                ArriveLogic();
-                break;
-            case "Order":
-                OrderLogic();
-                break;
-            case "Leave":
-                LeaveLogic();
-                break;
-            default:
-                GD.Print("StateMachineRuntime state variable not valid state!");
-                break;
-        }
-    }
+	//determines what happens when for each stage
+	public void stateLogic() {
+		//depending on the current state this decides which logic is going to play out.
+		switch (state) {
+			case "Arrive":
+				ArriveLogic();
+				break;
+			case "Order":
+				OrderLogic();
+				break;
+			case "Leave":
+				LeaveLogic();
+				break;
+			default:
+				GD.Print("StateMachineRuntime state variable not valid state!");
+				break;
+		}
+	}
 
-    //Moves the state machine to the next state. This also starts the logic for that state
-    public void NextState() {
-        //if we're at the end of the array then make the stateIndex 0 to restart the StateMachine
-        if (stateIndex != states.Count - 1) {
-            stateIndex++;
-        } else {
-            stateIndex = 0;
-            GD.Print("Reset StateMachineRuntime to start over!");
-        }
+	//Moves the state machine to the next state. This also starts the logic for that state
+	public void NextState() {
+		//if we're at the end of the array then make the stateIndex 0 to restart the StateMachine
+		if (stateIndex != states.Count - 1) {
+			stateIndex++;
+		} else {
+			stateIndex = 0;
+			GD.Print("Reset StateMachineRuntime to start over!");
+		}
 
-        //update the previousState and update the state with the new index value
-        previousState = state;
-        state = states[stateIndex];
-        stateLogic();
-    }
+		//update the previousState and update the state with the new index value
+		previousState = state;
+		state = states[stateIndex];
+		stateLogic();
+	}
 
-    private void ArriveLogic() {
-        //create a new character (maybe randomized to decide the image used)
-        Random rand = new Random();
-        int charType = rand.Next(0, 2);
-        string chType;
+	private void ArriveLogic() {
+		//create a new character (maybe randomized to decide the image used)
+		Random rand = new Random();
+		int charType = rand.Next(0, 2);
+		string chType;
 
-        switch (charType) {
-            case 0:
-                chType = "apollo.png";
-                break;
-            case 1:
-                chType = "serpent.png";
-                break;
-            case 2:
-                chType = "unicorn.png";
-                break;
-            default:
-                GD.Print("ArriveLogic Random Number not valid result! Defaulting to Apollo.");
-                chType = "apollo.png";
-                break;
-        }
+		switch (charType) {
+			case 0:
+				chType = "apollo.png";
+				break;
+			case 1:
+				chType = "serpent.png";
+				break;
+			case 2:
+				chType = "unicorn.png";
+				break;
+			default:
+				GD.Print("ArriveLogic Random Number not valid result! Defaulting to Apollo.");
+				chType = "apollo.png";
+				break;
+		}
 
-        Sprite charSprite = GetNode<Sprite>("CharacterSprite");
-        charSprite.Texture = ResourceLoader.Load("res://Images/Characters/" + chType) as Texture;
+		Sprite charSprite = GetNode<Sprite>("CharacterSprite");
+		charSprite.Texture = ResourceLoader.Load("res://Images/Characters/" + chType) as Texture;
 
-        //have the new character 'walk' on screen
-        MoveSprite(charSprite, chType);
+		//have the new character 'walk' on screen
+		MoveSprite(charSprite, chType);
 
-        //change to the next state
-        NextState();
-    }
+		//change to the next state
+		NextState();
+	}
 
-    private void OrderLogic() {
-        //run the Order() method in the OrderSys 
-    }
+	private void OrderLogic() {
+		//run the Order() method in the OrderSys 
+	}
 
-    private void LeaveLogic() {
-        //move the character off screen and then delete them
-        //add points to point total (only if we do a points thingy)
-        //change to the first state
-    }
+	private void LeaveLogic() {
+		//move the character off screen and then delete them
+		//add points to point total (only if we do a points thingy)
+		//change to the first state
+	}
 
-    async private void MoveSprite(Sprite ch, string chType) {
-        for (float i = ch.Transform.x.x, i < ) {
+	//async private void MoveSprite(Sprite ch, string chType) {
+		//for (float i = ch.Transform.x.x, i < ) {
 
-        }
-    }
+		//}
+	//}
 }
--- ff2D.gd
+++ ff2D.gd
@@ -1,20 +1,25 @@
 extends Sprite
 
+var Fname
 var fruitjuice = preload("res://Images/Drinks/dr_16_forbiddenFruitJuice.png")
 var normalfruit = preload("res://Images/Ingredients/in_08_forbiddenFruit.png")
 onready var fruit = get_node(".")
 var selected = false
 var rest_point
-var rest_nodesff = []
+var rest_nodes = []
 var main_point
 var main_nodes = []
+var mix_point
+var mix_nodes = []
 
 func _ready():
-	rest_nodesff = get_tree().get_nodes_in_group("zone")
+	rest_nodes = get_tree().get_nodes_in_group("zoneff")
 	main_nodes = get_tree().get_nodes_in_group("ffmain")
+	mix_nodes = get_tree().get_nodes_in_group("mixspot")
 	main_point = main_nodes[0].global_position
-	rest_point = rest_nodesff[0].global_position
-	rest_nodesff[0].select()
+	rest_point = rest_nodes[0].global_position
+	mix_point = mix_nodes[0].global_position
+	rest_nodes[0].select()
 
 func _physics_process(delta):
 	
@@ -26,16 +31,19 @@ func _physics_process(delta):
 
 func _on_ff_2D_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("grab"):
-		print(name)
-		print("working")
+		
+		Fname = name
+		print(Fname)
 		selected = true
+		
+	
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and not event.pressed:
 			selected = false
 			var shortest_dist = 75
-			for child in rest_nodesff:
+			for child in rest_nodes:
 				var distance = global_position.distance_to(child.global_position)
 				if distance < shortest_dist:
 					child.select()
@@ -44,4 +52,6 @@ func _input(event):
 					fruit.set_texture(fruitjuice)
 					if rest_point == main_point:
 						fruit.set_texture(normalfruit)
+					
+						
 		
